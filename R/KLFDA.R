3e05a4758f80598e8a332374dfa2ebf89500fe01
xinghuq <30358028+xinghuq@users.noreply.github.com>
2021-06-23 12:16:58 +0800

    Update KLFDA.R

--- R/KLFDA.R
+++ R/KLFDA.R
@@ -1,4 +1,65 @@
 
+kmatrixGauss=function (x, sigma = 1)
+{
+  x <- t(as.matrix(x))
+  d <- nrow(x)
+  n <- ncol(x)
+  X2 <- t(as.matrix(colSums(x^2)))
+  distance2 <- repmat(X2, n, 1) + repmat(t(X2), 1, n) - 2 *
+    t(x) %*% x
+  K <- exp(-distance2/(2 * sigma^2))
+  return(K)
+}
+
+
+
+KLFDA=function(kdata, y, r,  metric = c("weighted", "orthonormalized",
+                                    "plain"),tol=1e-5, knn = 6, reg = 0.001)
+{
+  require(lfda)
+  k=kdata
+  metric <- match.arg(metric)
+  y <- t(as.matrix(y))
+  n <- nrow(k)
+  if (is.null(r))
+    r <- n
+  tSb <- mat.or.vec(n, n)
+  tSw <- mat.or.vec(n, n)
+  for (i in unique(as.vector(t(y)))) {
+    Kcc <- k[y == i, y == i]
+    Kc <- k[, y == i]
+    nc <- nrow(Kcc)
+    Kccdiag <- diag(Kcc)
+    distance2 <- repmat(Kccdiag, 1, nc) + repmat(t(Kccdiag),
+                                                 nc, 1) - 2 * Kcc
+    A <- getAffinityMatrix(distance2, knn, nc)
+    Kc1 <- as.matrix(rowSums(Kc))
+    Z <- Kc %*% (repmat(as.matrix(colSums(A)), 1, n) * t(Kc)) -
+      Kc %*% A %*% t(Kc)
+    tSb <- tSb + (Z/n) + Kc %*% t(Kc) * (1 - nc/n) + Kc1 %*%
+      (t(Kc1)/n)
+    tSw <- tSw + Z/nc
+  }
+  K1 <- as.matrix(rowSums(k))
+  tSb <- tSb - K1 %*% t(K1)/n - tSw
+  tSb <- (tSb + t(tSb))/2
+  tSw <- (tSw + t(tSw))/2
+  F=tSb/tSw
+  require(lfda)
+  eigTmp <- suppressWarnings(rARPACK::eigs(A = solve(tSw + reg * diag(1, nrow(tSw), ncol(tSw)),tol=tol) %*% tSb, k = r,
+                                        which = "LM"))
+  eigVec <- Re(eigTmp$vectors)
+  eigVal <- as.matrix(Re(eigTmp$values))
+
+  Tr <- getMetricOfType(metric, eigVec, eigVal, n)
+  Z <- t(t(Tr) %*% k)
+  out <- list(T = Tr, Z = Z)
+  class(out) <- "KLFDA"
+  return(out)
+}
+
+
+
 
 
 
@@ -32,7 +93,7 @@ multinomial_kernel=function(data1,data2,order, beta, gamma){
 
 ###### This is a traditional implementation of KLFDA using self-defined classifier, see Pierre Enel (2019). Kernel Fisher Discriminant Analysis (https://www.github.com/p-enel/MatlabKFDA), GitHub. Retrieved March 29, 2019.
 
-KLFDA=function(X, Y, r,order, regParam, usekernel=TRUE,fL=0.5,priors,tol,reg,metric,plotFigures=FALSE,
+KLFDA_mk=function(X, Y, r,order, regParam, usekernel=TRUE,fL=0.5,priors,tol,reg,metric,plotFigures=FALSE,
               verbose,...){
   ## Kernel Local Fisher Discriminant Analysis
   # (with polynomial kernel)
